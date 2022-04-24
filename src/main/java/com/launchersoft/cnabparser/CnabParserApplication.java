cbdbeb5873ce37a562d472421696dc2875b343d7
Romulo <tp.romulo.domingos@totvspartners.com.br>
2022-04-24 11:20:27 -0300

    Create CNAB Controller

--- src/main/java/com/launchersoft/cnabparser/CnabParserApplication.java
+++ src/main/java/com/launchersoft/cnabparser/CnabParserApplication.java
@@ -2,8 +2,14 @@ package com.launchersoft.cnabparser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.launchersoft.cnabparser.configuration.CustomProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+    CustomProperties.class
+})
 public class CnabParserApplication {
 
     public static void main(String[] args) {
